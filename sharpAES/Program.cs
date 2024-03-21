aebb511f9ce9452a060ae8380885b20f7b0d2346
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-03-21 10:08:06 +0000

    Finished GetLevel function Added validation to the GetLevel function to avoid errors when people are stupid and put strings and characters in when the program asks for an integer

--- sharpAES/Program.cs
+++ sharpAES/Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 namespace sharpAes
 {
     public class SharpAes
     {
-        protected string plaintext = "";
+        protected string? plaintext = "";
         protected string password = "";
         protected int level = 0;
 
@@ -12,7 +13,21 @@ namespace sharpAes
         {
             Console.WriteLine("sharpAES v0.1");
             plaintext = GetPlainText();
+            level = GetLevel();
 
+        }
+
+        protected int GetLevel()
+        {
+            bool valid = false;
+            string value = "";
+            Console.WriteLine("To use AES-128, please enter 1. To use AES-192, please enter 2. To use AES-256, please enter 3. ");
+            value = Console.ReadLine()!;
+            while (valid == false)
+            {
+                valid = int.TryParse(value, out level);
+            }
+            return level;
         }
 
         protected string GetPlainText()
@@ -32,6 +47,16 @@ namespace sharpAes
                 }
             }
             return plaintext;
+        }
+
+        protected string GetPassword()
+        {
+            return "this is not secure now.";
+        }
+
+        protected void GenerateKey(string password)
+        {
+
         }
 
         protected string Encrypt()
