9720ccfc12d167f3145dbcfe7fd9fb241b8d221c
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-03-21 09:37:06 +0000

    Add project files.

---
+++ sharpAES/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+namespace sharpAes
+{
+    public class SharpAes
+    {
+        protected string plaintext = "";
+        protected string password = "";
+        protected int level = 0;
+
+        SharpAes() 
+        {
+            Console.WriteLine("sharpAES v0.1");
+            plaintext = GetPlainText();
+
+        }
+
+        protected string GetPlainText()
+        {
+            bool valid = false;
+            Console.WriteLine("Please enter the plaintext below: ");
+            while (valid == false)
+            {
+                plaintext = Console.ReadLine();
+                if (plaintext == null)
+                {
+                    Console.WriteLine("The plaintext cannot be null. Please enter valid plaintext below: ");
+                }
+                else
+                {
+                    Console.WriteLine("Plaintext accepted. ");
+                }
+            }
+            return plaintext;
+        }
+
+        protected string Encrypt()
+        {
+            Console.WriteLine("Temp");
+            return "hello";
+        }
+    }
+}
